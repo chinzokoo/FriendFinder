4ad24566d6014684fcb81b16166440e8938110dd
Lindsay Terry <lterry.development@gmail.com>
2018-11-16 20:58:55 -0800

    test

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friends.
+// Initially we just set it equal to a "dummy" friend.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var tableArray = [
+    {
+      name: "Ahmed",
+      photo: "https://www.wbworldabudhabi.com/en/characters/fred-flintstone",
+      question1: 3,
+      question2: 3,
+      question3: 3,
+      question4: 3,
+      question5: 3,
+      question6: 3,
+      question7: 3,
+      question8: 3,
+      question9: 3,
+      question10: 3
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = tableArray;
+  
